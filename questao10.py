05620b676245dcb2ae66f4a8e33373b4ca0c58f0
KatjaMateus <145811394+KatjaMateus@users.noreply.github.com>
2023-10-24 22:02:03 -0300

    questao10.py

---
+++ questao10.py
@@ -0,0 +1,6 @@
+for i in range(1,11):
+    numero = int(input("Digite um número: "))
+    if numero % 2 == 0:
+        print(f"{numero} é um número par")
+    else:
+        print(f"{numero} é um número impar")
