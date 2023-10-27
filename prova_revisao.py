7dc1f2a170790244f1369607eb56c7e4169de62a
KatjaMateus <145811394+KatjaMateus@users.noreply.github.com>
2023-10-26 22:01:37 -0300

    Add files via upload

---
+++ prova_revisao.py
@@ -0,0 +1,10 @@
+email = "sisu@gmail.com"
+senha = "sisu15"
+
+while True:
+    email = str(input("Digite seu email: "))
+    senha = str(input("Digite sua senha: "))
+    if email == "sisu@gmail.com" and senha == "sisu15":
+        break
+    else:
+        print("Tente novamente!")
